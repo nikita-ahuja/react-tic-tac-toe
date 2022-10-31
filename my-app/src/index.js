b56ce0ab91cfb58a58a7a0c7aacf636ab1fb8a00
Nikita Ahuja <nahuja038@gmail.com>
2022-10-30 23:29:22 -0500

    add onClick attribute to button tag, write basic function to log 'click' to console

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -6,7 +6,7 @@ class Square extends React.Component {
 
     render() {
       return (
-        <button className="square">
+        <button className="square" onClick={function() { console.log('click'); }}>
           {this.props.value}
         </button>
       );
